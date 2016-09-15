151d11903b2ef45ffd055463466c78805fc300bd
Altuğ Bilgin Altıntaş <altuga@Altugs-MacBook-Air.local>
2016-09-15 15:18:24 +0300

    Test Driven Development Example First example

---
+++ src/test/java/com/kodcu/TestOrderInformation.java
@@ -0,0 +1,18 @@
+package com.kodcu;
+
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestOrderInformation {
+
+    @Test
+    public void testCreateOrder() {
+        Person p = new Person();
+        p.setName("Ahmet");
+        assertEquals("ahmet", p1.getName());
+    }
+}
